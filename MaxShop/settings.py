1a6539f2f230a9b2549e704f9004378c26760e99
footballfrenzy <footballfrenzy@gmail.com>
2022-10-06 15:47:30 +0530

    my file

--- MaxShop/settings.py
+++ MaxShop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*qsmrw4p!u90ra#_2)9u$-zhksxaz!tbm05)t2yqqbj(u+%m_r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'MaxShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'footballfrenzy',
+        'USER': 'footballuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

891f388e99d4a2617d213c852873cc7d30549b0f
footballfrenzy <footballfrenzy@gmail.com>
2022-10-07 22:59:15 +0530

    change

--- MaxShop/settings.py
+++ MaxShop/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'MaxShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
  
